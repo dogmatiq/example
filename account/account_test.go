0f2ca3584943c3e6dc339184eb9c8caa48f96c6e
James Harris <mailjamesharris@gmail.com>
2019-02-01 15:18:12 +1000

    Be explicit about the command-under-test.

--- account/account_test.go
+++ account/account_test.go
@@ -9,18 +9,16 @@ import (
 )
 
 func TestAccount_OpenAccount(t *testing.T) {
-	cmd := messages.OpenAccount{
-		AccountID: "A001",
-		Name:      "Bob Jones",
-	}
-
 	t.Run(
 		"it opens the account",
 		func(t *testing.T) {
 			testrunner.Runner.
 				Begin(t).
 				ExecuteCommand(
-					cmd,
+					messages.OpenAccount{
+						AccountID: "A001",
+						Name:      "Bob Jones",
+					},
 					EventRecorded(
 						messages.AccountOpened{
 							AccountID: "A001",
@@ -34,6 +32,11 @@ func TestAccount_OpenAccount(t *testing.T) {
 	t.Run(
 		"it does not open an account that is already open",
 		func(t *testing.T) {
+			cmd := messages.OpenAccount{
+				AccountID: "A001",
+				Name:      "Bob Jones",
+			}
+
 			testrunner.Runner.
 				Begin(t).
 				Setup(cmd).
--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module github.com/dogmatiq/example
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dogmatiq/dogma v0.0.0-20190125031156-7aa035456afb
-	github.com/dogmatiq/dogmatest v0.0.0-20190131050056-6cbc714137eb
+	github.com/dogmatiq/dogmatest v0.0.0-20190201051008-9353905c0391
 	github.com/dogmatiq/enginekit v0.0.0-20190130234730-a07e2b8a259b
-	github.com/kr/pty v1.1.3 // indirect
-	github.com/stretchr/objx v0.1.1 // indirect
-	golang.org/x/sys v0.0.0-20190130150945-aca44879d564 // indirect
 )
