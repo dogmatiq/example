0fb479e269204b20e1c06b2c86eb4b28976e84f5
Danil Petrov <danil@codeworx.com.au>
2019-02-11 21:40:24 +1000

    include grommet components - wip

--- www/src/App/App.jsx
+++ www/src/App/App.jsx
@@ -1,12 +1,39 @@
 import React from 'react';
 import { Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Box, Button, Heading, Grommet } from 'grommet';
+import { Notification } from 'grommet-icons';
 
 import { history } from '../_helpers';
 import { alertActions } from '../_actions';
 import { PrivateRoute } from '../_components';
 import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
+
+
+const AppBar = (props) => (
+    <Box
+        tag='header'
+        direction='row'
+        align='center'
+        justify='between'
+        background='brand'
+        pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+        elevation='medium'
+        style={{ zIndex: '1' }}
+        {...props}
+    />
+);
+
+const theme = {
+    global: {
+      font: {
+        family: 'Roboto',
+        size: '14px',
+        height: '20px',
+      },
+    },
+  };
 
 class App extends React.Component {
     constructor(props) {
@@ -22,21 +49,20 @@ class App extends React.Component {
     render() {
         const { alert } = this.props;
         return (
-            <div className="jumbotron">
-                <div className="container">
-                    <div className="col-sm-8 col-sm-offset-2">
-                        {alert.message &&
-                            <div className={`alert ${alert.type}`}>{alert.message}</div>
-                        }
-                        <Router history={history}>
-                            <div>
-                                <PrivateRoute exact path="/" component={HomePage} />
-                                <Route path="/login" component={LoginPage} />
-                            </div>
-                        </Router>
-                    </div>
-                </div>
-            </div>
+            <Grommet theme={theme}>
+                <Box fill>
+                    <AppBar>
+                        <Heading level='3' margin='none'>Dogma Banking Example</Heading>
+                        <Button icon={<Notification />} onClick={() => { }} />
+                    </AppBar>
+                    <Router history={history}>
+                        <div>
+                            <PrivateRoute exact path="/" component={HomePage} />
+                            <Route path="/login" component={LoginPage} />
+                        </div>
+                    </Router>
+                </Box>
+            </Grommet>
         );
     }
 }
@@ -49,4 +75,4 @@ function mapStateToProps(state) {
 }
 
 const connectedApp = connect(mapStateToProps)(App);
-export { connectedApp as App }; 
+export { connectedApp as App };
