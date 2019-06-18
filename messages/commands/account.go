7da6e3a0a3e4cdf8b0ea1bdd8ca25cc6556d1305
Kevin Millar <koden.km@gmail.com>
2019-06-18 19:13:23 +1000

    Move command OpenAccountForNewCustomer to account file as discussed.

--- messages/commands/account.go
+++ messages/commands/account.go
@@ -1,5 +1,15 @@
 package commands
 
+// OpenAccountForNewCustomer is a command requesting that a new bank account be
+// opened for a new customer.
+type OpenAccountForNewCustomer struct {
+	CustomerID    string
+	CustomerName  string
+	CustomerEmail string
+	AccountID     string
+	AccountName   string
+}
+
 // OpenAccount is a command requesting that a new bank account be opened for an
 // existing customer.
 type OpenAccount struct {
--- messages/commands/customer.go
+++ messages/commands/customer.go
@@ -1,15 +1,5 @@
 package commands
 
-// OpenAccountForNewCustomer is a command requesting that a new bank account be
-// opened for a new customer.
-type OpenAccountForNewCustomer struct {
-	CustomerID    string
-	CustomerName  string
-	CustomerEmail string
-	AccountID     string
-	AccountName   string
-}
-
 type ChangeCustomerEmailAddress struct {
 	CustomerID    string
 	CustomerEmail string
