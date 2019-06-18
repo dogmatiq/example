7479fb37badf9e9ec6333d0206b44bfa5a51c208
Kevin Millar <koden.km@gmail.com>
2019-06-18 19:14:08 +1000

    Add missing doc block.

--- messages/commands/customer.go
+++ messages/commands/customer.go
@@ -1,5 +1,7 @@
 package commands
 
+// ChangeCustomerEmailAddress is a command requesting that a customer email
+// address be changed.
 type ChangeCustomerEmailAddress struct {
 	CustomerID    string
 	CustomerEmail string
