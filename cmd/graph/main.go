7831bab1502c360a804eaaa33c834713ad143371
James Harris <mailjamesharris@gmail.com>
2019-12-02 13:31:12 +1000

    Update to graphkit v0.3.0.

--- cmd/graph/main.go
+++ cmd/graph/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 
+	"github.com/dogmatiq/configkit"
 	"github.com/dogmatiq/example"
 	"github.com/dogmatiq/graphkit"
 )
 
 func main() {
-	g, err := graphkit.Generate(&example.App{})
+	app, err := example.NewApp(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	cfg := configkit.FromApplication(app)
+
+	g, err := graphkit.Generate(cfg)
 	if err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/dogmatiq/example
 go 1.13
 
 require (
-	github.com/dogmatiq/dogma v0.6.0
-	github.com/dogmatiq/graphkit v0.2.0
+	github.com/dogmatiq/configkit v0.1.0
+	github.com/dogmatiq/dogma v0.6.1
+	github.com/dogmatiq/graphkit v0.3.0
 	github.com/dogmatiq/projectionkit v0.3.0
 	github.com/dogmatiq/testkit v0.3.0
 	github.com/mattn/go-sqlite3 v1.11.0
