91326ee4c0010a1bb5a172af569ef30f5de04997
Kevin Millar <koden.km@gmail.com>
2019-12-17 10:37:26 +1000

    Setup basic menu system

--- cmd/bank/main.go
+++ cmd/bank/main.go
@@ -1,20 +1,43 @@
 package main
 
 import (
-	"context"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 	"time"
 
-	"github.com/dogmatiq/dogma"
 	"github.com/dogmatiq/example"
 	"github.com/dogmatiq/example/database"
 	"github.com/dogmatiq/example/messages"
-	"github.com/dogmatiq/example/messages/commands"
 	"github.com/dogmatiq/testkit/engine"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func businessDayFromTime(t time.Time) string {
 	return t.Format(messages.BusinessDateFormat)
+}
+
+func readString(reader *bufio.Reader) string {
+	text, _ := reader.ReadString('\n')
+	return strings.Replace(text, "\n", "", -1)
+}
+
+type appState struct {
+	input  *bufio.Reader
+	engine *engine.Engine
+	time   time.Time
+}
+
+type menuItem struct {
+	// option is the short menu item code, usually a single character.
+	option string
+
+	// description is the description of the menu item.
+	description string
+
+	// command is the command to return when this menu item is selected.
+	command string
 }
 
 func main() {
@@ -31,60 +54,72 @@ func main() {
 		panic(err)
 	}
 
-	messages := []dogma.Message{
-		commands.OpenAccountForNewCustomer{
-			CustomerID:   "cust1",
-			CustomerName: "Anna Smith",
-			AccountID:    "acct1",
-			AccountName:  "Anna Smith",
+	as := appState{
+		input:  bufio.NewReader(os.Stdin),
+		engine: en,
+		time:   time.Date(2001, 10, 20, 11, 22, 33, 44, time.UTC),
+	}
+
+	fmt.Println()
+	fmt.Println("Dogma Example Bank")
+	fmt.Println("==================")
+
+	as.runMainMenu()
+}
+
+// showMenu shows a title and a list of options.
+func (as appState) showMenu(title string, items []menuItem) string {
+	fmt.Println()
+	fmt.Printf("Time: %s", as.time)
+	fmt.Println()
+	fmt.Println()
+	fmt.Println(title)
+	fmt.Println("--------------------")
+	fmt.Println()
+	fmt.Println("Options:")
+	for _, item := range items {
+		fmt.Printf("  [%s] - %s\n", item.option, item.description)
+	}
+
+	var input string
+	for {
+		fmt.Println()
+		fmt.Print("Choice: ")
+		input = readString(as.input)
+
+		for _, item := range items {
+			if item.option == input {
+				return item.command
+			}
+		}
+
+		fmt.Printf("Invalid selection: %s\n", input)
+	}
+}
+
+func (as appState) runMainMenu() {
+	mainMenuItems := []menuItem{
+		{
+			option:      "h",
+			description: "Say Hello World!",
+			command:     "hello",
 		},
-		commands.OpenAccountForNewCustomer{
-			CustomerID:   "cust2",
-			CustomerName: "Bob Jones",
-			AccountID:    "acct2",
-			AccountName:  "Bob Jones",
-		},
-		commands.Deposit{
-			TransactionID: "txn1",
-			AccountID:     "acct1",
-			Amount:        10000,
-		},
-		commands.Withdraw{
-			TransactionID: "txn2",
-			AccountID:     "acct1",
-			Amount:        500,
-			ScheduledDate: businessDayFromTime(time.Now()),
-		},
-		commands.Transfer{
-			TransactionID: "txn3",
-			FromAccountID: "acct1",
-			ToAccountID:   "acct2",
-			Amount:        2500,
-			ScheduledDate: businessDayFromTime(time.Now()),
-		},
-		commands.Transfer{
-			TransactionID: "txn4",
-			FromAccountID: "acct1",
-			ToAccountID:   "acct2",
-			Amount:        500,
-			ScheduledDate: businessDayFromTime(time.Now().AddDate(0, 0, 1)),
+		{
+			option:      "x",
+			description: "Exit banking sytem",
+			command:     "exit",
 		},
 	}
 
-	for _, m := range messages {
-		err := en.Dispatch(
-			context.Background(),
-			m,
-			// engine.WithObserver(
-			// 	fact.ObserverFunc(func(f fact.Fact) {
-			// 		dapper.Print(f)
-			// 		fmt.Print("\n\n")
-			// 	}),
-			// ),
-			engine.EnableProjections(true),
-		)
-		if err != nil {
-			panic(err)
+	for {
+		switch as.showMenu("Main Menu", mainMenuItems) {
+		case "exit":
+			fmt.Println()
+			fmt.Println("Goodbye")
+			return
+		case "hello":
+			fmt.Println()
+			fmt.Println("Hello World!")
 		}
 	}
 }
