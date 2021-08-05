78c7078e5a4237c538c67fab397db316e9dad5f5
James Harris <mailjamesharris@gmail.com>
2021-08-05 19:54:24 +1000

    Remove unused projections.

--- app.go
+++ app.go
@@ -5,12 +5,7 @@ import (
 
 	"github.com/dogmatiq/dogma"
 	"github.com/dogmatiq/example/domain"
-	"github.com/dogmatiq/example/projections"
-	"github.com/dogmatiq/projectionkit/sqlprojection"
 )
-
-// AppKey is the example application's identity key.
-const AppKey = "22028264-0bca-43e1-8d9d-cd094efb10b7"
 
 // App is a dogma.Application implementation for the example "bank" domain.
 type App struct {
@@ -24,9 +19,6 @@ type App struct {
 	TransferProcess                  domain.TransferProcessHandler
 	WithdrawalProcess                domain.WithdrawalProcessHandler
 
-	AccountProjection  projections.AccountProjectionHandler
-	CustomerProjection projections.CustomerProjectionHandler
-
 	// ReadDB is the database to use for read-models. If it is nil the
 	// projection message handlers are omitted from the application
 	// configuration.
@@ -35,7 +27,7 @@ type App struct {
 
 // Configure configures the Dogma engine for this application.
 func (a *App) Configure(c dogma.ApplicationConfigurer) {
-	c.Identity("bank", AppKey)
+	c.Identity("bank", "22028264-0bca-43e1-8d9d-cd094efb10b7")
 
 	c.RegisterAggregate(a.AccountAggregate)
 	c.RegisterAggregate(a.CustomerAggregate)
@@ -46,19 +38,4 @@ func (a *App) Configure(c dogma.ApplicationConfigurer) {
 	c.RegisterProcess(a.OpenAccountForNewCustomerProcess)
 	c.RegisterProcess(a.TransferProcess)
 	c.RegisterProcess(a.WithdrawalProcess)
-
-	if a.ReadDB != nil { // TODO: Remove this when testkit is updated
-		c.RegisterProjection(
-			sqlprojection.New(
-				a.ReadDB,
-				&a.AccountProjection,
-			),
-		)
-		c.RegisterProjection(
-			sqlprojection.New(
-				a.ReadDB,
-				&a.CustomerProjection,
-			),
-		)
-	}
 }
